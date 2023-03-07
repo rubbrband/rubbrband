eee43a7e91843a9c8a34f4707f852fd49e1590a9
root <abhinav@rubbrband.com>
2023-03-07 00:46:34 +0000

    bugfix

--- rubbrband/controllers/models/sd_webui/web.sh
+++ rubbrband/controllers/models/sd_webui/web.sh
@@ -1,3 +1,3 @@
 docker run --name rb-sd_webui --gpus all -it -d -v $SCRIPT_DIR/sd-v1-4-full-ema.ckpt:/home/engineering/sd-v1-4-full-ema.ckpt -v $dataset_dir:/home/engineering/dataset-dir -d rubbrband/dreambooth:latest 
 
-docker exec -it rb-dreambooth /bin/bash -c "webui.sh --xformers --share --enable-insecure-extension-access"
+docker exec -it rb-sd_webui /bin/bash -c "webui.sh --xformers --share --enable-insecure-extension-access"
