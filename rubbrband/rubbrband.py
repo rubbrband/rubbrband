cc9599ea0d6627c81152b7b089b1185469f1ae3b
Jeremy Lee <jrmylee@berkeley.edu>
2023-05-09 13:26:26 -0700

    eval

--- rubbrband/rubbrband.py
+++ rubbrband/rubbrband.py
@@ -1,3 +1,5 @@
+import requests
+
 _api_key = None
 
 
@@ -6,15 +8,20 @@ def set_api_key(api_key):
     _api_key = api_key
 
 
-def start_job(endpoint, payload):
+def start_job(prompt):
     if _api_key is None:
         raise ValueError("API key is not set. Please set the API key using set_api_key() function.")
 
-    # headers = {
-    #     'Content-Type': 'application/json',
-    #     'Authorization': f'Bearer {_api_key}'
-    # }
+    endpoint = "http://localhost:4200/add_job"
 
-    # response = requests.post(endpoint, json=payload, headers=headers)
-    # response.raise_for_status()
+    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {_api_key}"}
+
+    payload = {
+        "api_key": _api_key,
+        "prompt": prompt,
+    }
+
+    response = requests.post(endpoint, json=payload, headers=headers)
+    response.raise_for_status()
+    print(response)
     return True
