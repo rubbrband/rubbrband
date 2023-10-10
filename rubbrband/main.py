ce9de99810e21b7185405a05f0d198a0c12abbbc
Jeremy Lee <jrmylee@berkeley.edu>
2023-10-10 07:34:20 -0700

    rubbrband 0.2.15 - add instant evals

--- rubbrband/main.py
+++ rubbrband/main.py
@@ -1,3 +1,4 @@
+import base64
 import io
 import os
 import re
@@ -39,10 +40,74 @@ def image_to_jpg_byte_array(image):
     return jpg_byte_arr
 
 
+def jpg_byte_array_to_b64(jpg_byte_arr):
+    """Convert a JPEG byte array to a base64 string"""
+    return base64.b64encode(jpg_byte_arr).decode("utf-8")
+
+
 def is_url(string):
     """Check if a string is a valid URL"""
     regex_pattern = r"^(https?|ftp):\/\/([^\s/$.?#].[^\s]*)$"
     return bool(re.match(regex_pattern, string))
+
+
+def eval(image, features=[], prompt="No prompt", metadata={}):
+    """Evaluate an image and return the response"""
+    image_url = None
+
+    # Handle image input
+    if isinstance(image, str) and is_url(image):
+        # If image is a url, download it
+        image_url = image
+    elif isinstance(image, str) and os.path.isfile(image):
+        # If image is a file path, read it
+        with open(image, "rb") as file:
+            image = file.read()
+    elif isinstance(image, (Image.Image, bytes)):
+        pass
+    else:
+        print("Invalid image type")
+        return False
+
+    if image_url:
+        response = requests.post(
+            "https://api.rubbrband.com/eval",
+            json={
+                "metadata": metadata,
+                "prompt": prompt,
+                "image_url": image_url,
+                "api_key": api_key,
+                "features": features,
+            },
+            headers={
+                "Content-Type": "application/json",
+            },
+            timeout=5,
+        )
+    else:
+        image = image_to_jpg_byte_array(image)
+        base64_image = jpg_byte_array_to_b64(image)
+        response = requests.post(
+            "https://api.rubbrband.com/eval",
+            json={
+                "metadata": metadata,
+                "prompt": prompt,
+                "image": base64_image,
+                "api_key": api_key,
+                "features": features,
+            },
+            headers={
+                "Content-Type": "application/json",
+            },
+            timeout=5,
+        )
+
+    if response is None:
+        return False
+
+    response = response.json()
+
+    return response
 
 
 def upload(image, prompt, metadata={}):
