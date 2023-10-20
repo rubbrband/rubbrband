d836ed41f2eaefbe07ae427ad38f7330dc767a72
Jeremy Lee <jrmylee@berkeley.edu>
2023-10-19 21:14:32 -0700

    0.2.19 - minor fix

--- rubbrband/__init__.py
+++ rubbrband/__init__.py
@@ -1,3 +1,3 @@
-from .main import eval, get_image_metadata, init, upload, vote_on_image
+from .main import eval, init, vote_on_image
 
-__all__ = ["get_image_metadata", "init", "upload", "vote_on_image", "eval"]
+__all__ = ["init", "vote_on_image", "eval"]
